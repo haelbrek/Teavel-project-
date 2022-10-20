9ecc43814d4a5b89212cb388fde116e7fda66dbb
Adeline El Khoury <adelineinati@gmail.com>
2022-10-20 15:04:05 +0200

    branches

---
+++ travel_project/result_preference/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def result_preference(request):
+
+    
+    return render(request, 'result_preference/result_preference.html')
--- travel_project/travel_project/settings.py
+++ travel_project/travel_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'result_preference'
+
 ]
 
 MIDDLEWARE = [
