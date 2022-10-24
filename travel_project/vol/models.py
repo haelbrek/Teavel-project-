049ddb30148c10e5a0da50ae02385cd2878f52e8
Hamza <Hamzaelbrek3@gmail.com>
2022-10-24 16:53:35 +0200

    formulaire vol

--- travel_project/vol/models.py
+++ travel_project/vol/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Vols(models.Model):
+
+    depart = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )   
+    destination = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )  
+    date_depart = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )  
+    date_arrive = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )  
+    
+    classe_vol = models.CharField(
+        max_length=3,
+        blank= False
+    )
+
+    
