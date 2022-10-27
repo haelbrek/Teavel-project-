1978333ea417233672d61cb57530378767d78457
Hamza <Hamzaelbrek3@gmail.com>
2022-10-27 16:44:40 +0200

    sign up

--- travel_project/divers/urls.py
+++ travel_project/divers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path ( 'contact/', views.contact_page,  name="contact"),
-    path ('about/', views.about_page, name="about")
+    path ('about/', views.about_page, name="about"),
+    path('signup/', views.SignupPage.as_view(), name='signup'),
 ]
