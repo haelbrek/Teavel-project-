067f25ce1a801e85fed4e69f79582946c8c27542
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2022-10-28 11:35:29 +0200

    app hotel en cours

--- travel_project/hotel/models.py
+++ travel_project/hotel/models.py
@@ -1,3 +1,11 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Hotels(models.Model):
+
+    ville = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )
